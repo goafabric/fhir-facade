ba89a59a836ac11f63fc82f4529f5894ca97a3eb
Andreas Mautsch <amautsch>
2021-03-30 13:50:36 +0200

    changes

---
+++ build-native.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git pull
+time mvn clean spring-boot:build-image -P docker-image-native
+#time mvn clean spring-boot:build-image install -P docker-image-native
+docker run --name spring-boot-example-graal-native --rm -p50800:50800 goafabric/example-fhir-service-native:1.0.0-SNAPSHOT
--- doc/todo.md
+++ doc/todo.md
@@ -2,5 +2,6 @@
 - builder für patient
 
 - builder für search
-
+                   
+#spring native error
 docker run --name example-fhir --rm -p50300:50300 goafabric/example-fhir-service-arm64v8:1.0.0-SNAPSHOT
