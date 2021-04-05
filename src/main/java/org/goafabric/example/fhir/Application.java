318a50591fb99bb0e0399ece4c86b166335e2468
Andreas Mautsch <amautsch>
2021-04-05 13:38:06 +0200

    changes

--- src/main/java/org/goafabric/example/fhir/Application.java
+++ src/main/java/org/goafabric/example/fhir/Application.java
@@ -2,8 +2,13 @@ package org.goafabric.example.fhir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
+@TypeHint(typeNames = {
+        "org.hl7.fhir.dstu3.hapi.ctx.FhirDstu3"
+}, access = AccessBits.ALL)
 public class Application {
 
     public static void main(String[] args) {
