2ed94bbcaa37580072c264b47ce6a07b44c094f4
Andreas Mautsch <amautsch>
2021-06-22 16:08:09 +0200

    exceptionhandler added

--- src/main/java/org/goafabric/example/fhir/configuration/FhirProperties.java
+++ src/main/java/org/goafabric/example/fhir/configuration/FhirProperties.java
@@ -22,85 +22,25 @@ package org.goafabric.example.fhir.configuration;
 
 
 import ca.uhn.fhir.context.FhirVersionEnum;
-
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "hapi.fhir")
+@Data
 public class FhirProperties {
-
     private FhirVersionEnum version = FhirVersionEnum.DSTU3;
-
     private Server server = new Server();
-
     private Validation validation = new Validation();
 
-    public FhirVersionEnum getVersion() {
-        return version;
-    }
-
-    public void setVersion(FhirVersionEnum version) {
-        this.version = version;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
-
-    public Validation getValidation() {
-        return validation;
-    }
-
-    public void setValidation(Validation validation) {
-        this.validation = validation;
-    }
-
+    @Data
     public static class Server {
-
         private String url;
-
         private String path = "/fhir/*";
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        public String getPath() {
-            return path;
-        }
-
-        public void setPath(String path) {
-            this.path = path;
-        }
     }
 
+    @Data
     public static class Validation {
-
         private boolean enabled = true;
-
         private boolean requestOnly = true;
-
-        public boolean isEnabled() {
-            return enabled;
-        }
-
-        public void setEnabled(boolean enabled) {
-            this.enabled = enabled;
-        }
-
-        public boolean isRequestOnly() {
-            return requestOnly;
-        }
-
-        public void setRequestOnly(boolean requestOnly) {
-            this.requestOnly = requestOnly;
-        }
     }
 }
