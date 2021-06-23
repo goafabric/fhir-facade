18b76ff640941be64e56de700039317dd14b1a4c
Andreas Mautsch <amautsch>
2021-06-23 16:46:14 +0200

    adapter added

--- src/main/java/org/goafabric/example/fhir/crossfunctional/TenantIdInterceptor.java
+++ src/main/java/org/goafabric/example/fhir/crossfunctional/TenantIdInterceptor.java
@@ -9,10 +9,22 @@ import lombok.extern.slf4j.Slf4j;
 @Interceptor
 @Slf4j
 public class TenantIdInterceptor {
+    private static final ThreadLocal<String> tenantIdThreadLocal = new ThreadLocal<>();
 
     @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
-    public void logRequests(RequestDetails theRequest) {
-        log.info("Request of type {} with request ID: {}", theRequest.getOperation(), theRequest.getRequestId());
+    public void preHandle(RequestDetails request) {
+        final String tenantId = request.getHeader("X-TenantId");
+        log.info("#interceptor got tenant id {}", tenantId);
+        tenantIdThreadLocal.set(request.getHeader("X-TenantId"));
     }
 
+    @Hook(Pointcut.SERVER_PROCESSING_COMPLETED)
+    public void afterCompletion(RequestDetails request) {
+        tenantIdThreadLocal.remove();
+    }
+
+    public static String getTenantId() {
+        final String tenantId = tenantIdThreadLocal.get();
+        return tenantId == null ? "0" : tenantId;  //Todo: should throw exception
+    }
 }
