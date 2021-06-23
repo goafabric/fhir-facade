54f02b86320a6390b12f5ac303e2ce27359a489c
Andreas Mautsch <amautsch>
2021-06-23 13:59:15 +0200

    authentication added

--- src/main/java/org/goafabric/example/fhir/Application.java
+++ src/main/java/org/goafabric/example/fhir/Application.java
@@ -1,9 +1,12 @@
 package org.goafabric.example.fhir;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
