7c81a389e432f6c2cc66024f66ebba9bbab6c943
Andreas Mautsch <amautsch>
2021-06-27 12:29:30 +0200

    upgrade to 1.0.3

--- build-native.sh
+++ build-native.sh
@@ -2,4 +2,4 @@
 git pull
 time mvn clean spring-boot:build-image -P docker-image-native
 #time mvn clean spring-boot:build-image install -P docker-image-native
-docker run --name spring-boot-example-graal-native --rm -p50800:50800 goafabric/example-fhir-service-native:1.0.2-SNAPSHOT
+docker run --name spring-boot-example-graal-native --rm -p50800:50800 goafabric/example-fhir-service-native:1.0.3-SNAPSHOT
--- doc/release-notes.md
+++ doc/release-notes.md
@@ -1,3 +1,5 @@
+#1.0.3
+
 #1.0.2
 - DurationLogger added
 - Profiles added for Mock
