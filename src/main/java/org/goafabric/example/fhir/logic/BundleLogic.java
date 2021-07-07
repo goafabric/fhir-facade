e9df4827e6008465a8811e713f947cfaaba3f558
Andreas Mautsch <amautsch>
2021-07-07 14:19:35 +0200

    Bundleservice added

--- src/main/java/org/goafabric/example/fhir/logic/BundleLogic.java
+++ src/main/java/org/goafabric/example/fhir/logic/BundleLogic.java
@@ -3,15 +3,29 @@ package org.goafabric.example.fhir.logic;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.IdType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BundleLogic {
+    @Autowired
+    private PatientLogic patientLogic;
+
+    @Autowired
+    private PractitionerLogic practitionerLogic;
+
     public Bundle getBundle(IdType idType) {
         if (!"1".equals(idType.getIdPart())) {
             throw new ResourceNotFoundException("bundle not found");
         }
 
-        Bundle bundle = new Bundle();
-        //bundle.addEntry(new Bundle.BundleEntryComponent().)
-        return null;
+        final Bundle bundle = new Bundle();
+        bundle.addEntry(new Bundle.BundleEntryComponent()
+                .setResource(patientLogic.getPatient(idType)));
+
+        bundle.addEntry(new Bundle.BundleEntryComponent()
+                .setResource(practitionerLogic.getPractitioner(idType)));
+
+        return bundle;
     }
 }
--- src/main/java/org/goafabric/example/fhir/service/BundleService.java
+++ src/main/java/org/goafabric/example/fhir/service/BundleService.java
@@ -1,5 +1,6 @@
 package org.goafabric.example.fhir.service;
 
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.jaxrs.server.AbstractJaxRsResourceProvider;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Read;
@@ -7,10 +8,16 @@ import org.goafabric.example.fhir.logic.BundleLogic;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.IdType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BundleService extends AbstractJaxRsResourceProvider<Bundle> {
     @Autowired
     private BundleLogic bundleLogic;
+
+    public BundleService(FhirContext fhirContext) {
+        super(fhirContext);
+    }
 
     @Override
     public Class<Bundle> getResourceType() {
