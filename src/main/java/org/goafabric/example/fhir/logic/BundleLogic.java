49912fe982cc654155f2b4a893e146c3120149e5
Andreas Mautsch <amautsch>
2021-07-07 15:04:43 +0200

    Bundleservice added

--- src/main/java/org/goafabric/example/fhir/logic/BundleLogic.java
+++ src/main/java/org/goafabric/example/fhir/logic/BundleLogic.java
@@ -1,10 +1,7 @@
 package org.goafabric.example.fhir.logic;
 
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.IdType;
-import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Practitioner;
+import org.hl7.fhir.r4.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,18 +19,17 @@ public class BundleLogic {
         }
 
         final Bundle bundle = new Bundle();
-
-        final Bundle.BundleEntryComponent patientEntry = new Bundle.BundleEntryComponent();
-        patientEntry.setResource(patientLogic.getPatient(idType));
-        patientEntry.setFullUrl(Patient.class.getSimpleName() + "/" + idType.getIdPart());
-        bundle.addEntry(patientEntry);
-
-        final Bundle.BundleEntryComponent practitionerEntry = new Bundle.BundleEntryComponent();
-        practitionerEntry.setResource(practitionerLogic.getPractitioner(idType));
-        practitionerEntry.setFullUrl(Practitioner.class.getSimpleName() + "/" + idType.getIdPart());
-        bundle.addEntry(practitionerEntry);
+        bundle.addEntry(createBundleEntry(patientLogic.getPatient(idType)));
+        bundle.addEntry(createBundleEntry(practitionerLogic.getPractitioner(idType)));
 
         return bundle;
+    }
+
+    private Bundle.BundleEntryComponent createBundleEntry(Resource resource) {
+        final Bundle.BundleEntryComponent bundleEntry = new Bundle.BundleEntryComponent();
+        bundleEntry.setResource(resource);
+        bundleEntry.setFullUrl(resource.getClass().getSimpleName() + "/" + resource.getId());
+        return bundleEntry;
     }
 
 }
