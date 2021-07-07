d04964857bcbdfa3444efbf37e5ba77686cee695
Andreas Mautsch <amautsch>
2021-07-07 15:48:36 +0200

    tests added

---
+++ src/test/java/org/goafabric/example/fhir/service/BundleServiceIT.java
@@ -0,0 +1,27 @@
+package org.goafabric.example.fhir.service;
+
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.hl7.fhir.r4.model.Bundle;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BundleServiceIT {
+    @LocalServerPort
+    private String port;
+
+    @Test
+    void getBundle() {
+        final IGenericClient client = ClientFactory.createClient(port);
+
+        final Bundle Bundle =
+                client.read()
+                        .resource(Bundle.class)
+                        .withId("1").execute();
+
+        assertThat(Bundle).isNotNull();
+    }
+}
---
+++ src/test/java/org/goafabric/example/fhir/service/ClientFactory.java
@@ -0,0 +1,32 @@
+package org.goafabric.example.fhir.service;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IClientInterceptor;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.api.IHttpRequest;
+import ca.uhn.fhir.rest.client.api.IHttpResponse;
+
+import java.io.IOException;
+import java.util.Base64;
+
+public class ClientFactory {
+    private ClientFactory() {
+    }
+
+    public static IGenericClient createClient(String port) {
+        final IGenericClient client = FhirContext.forR4().newRestfulGenericClient(
+                "http://localhost:" + port + "/fhir");
+        client.registerInterceptor(new IClientInterceptor() {
+            @Override
+            public void interceptRequest(IHttpRequest theRequest) {
+                theRequest.addHeader("Authorization", "Basic "
+                        + Base64.getEncoder().encodeToString("admin:admin".getBytes()));
+            }
+
+            @Override
+            public void interceptResponse(IHttpResponse theResponse) throws IOException {
+            }
+        });
+        return client;
+    }
+}
