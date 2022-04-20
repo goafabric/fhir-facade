7d6676c341708104cd4f62572c6663588e37110b
andreas <andreas>
2022-04-20 15:46:31 +0200

    security fix

--- src/main/java/org/goafabric/fhir/configuration/SecurityConfiguration.java
+++ src/main/java/org/goafabric/fhir/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
-package org.goafabric.example.fhir.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+package org.goafabric.personservice.crossfunctional;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,11 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration//(proxyBeanMethods = false)
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@ConditionalOnProperty(value = "security.authentication.enabled", matchIfMissing = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Value("${security.authentication.enabled:true}")
+    private Boolean isAuthenticationEnabled;
+
     @Override //in memory authentication
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
@@ -29,17 +32,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-                .antMatchers(
-                        "/actuator/**",
-                        "/", "/welcome/**"
-                ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        if (isAuthenticationEnabled) {
+            httpSecurity.authorizeRequests()
+                    .antMatchers(
+                            "/actuator/**",
+                            "/", "/welcome/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        } else {
+            httpSecurity.authorizeRequests().anyRequest().permitAll();
+        }
     }
 
     @Bean
