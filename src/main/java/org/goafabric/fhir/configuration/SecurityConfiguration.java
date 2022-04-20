efa0c339a2967ee721aa94a3ca69afe448d70f7e
andreas <andreas>
2022-04-20 16:10:48 +0200

    security fix

--- src/main/java/org/goafabric/fhir/configuration/SecurityConfiguration.java
+++ src/main/java/org/goafabric/fhir/configuration/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package org.goafabric.fhir.configuration;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,8 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
@@ -20,20 +18,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override //in memory authentication
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("admin")
-                .password("$2a$10$X3nRHu5n2X0Ibny6fWJJvOFbdmrbZOmkTZbWv9001G8HY6LEPxWei")
-                .roles("standard_role");
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin").password("admin").roles("standard_role");
     }
 
     @Override
     protected void configure(final HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers(
-                        "/actuator/**",
-                        "/", "/welcome/**"
-                ).permitAll()
+
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
@@ -42,8 +33,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
