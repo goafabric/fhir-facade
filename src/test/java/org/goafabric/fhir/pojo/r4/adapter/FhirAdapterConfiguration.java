f8bcc255e8c0b3a1221f76f851c3d4b6da7e8eb1
andreas <andreas>
2022-05-02 14:42:40 +0200

    Organization added

--- src/test/java/org/goafabric/fhir/pojo/r4/adapter/FhirAdapterConfiguration.java
+++ src/test/java/org/goafabric/fhir/pojo/r4/adapter/FhirAdapterConfiguration.java
@@ -2,12 +2,23 @@ package org.goafabric.fhir.pojo.r4.adapter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @Configuration
 public class FhirAdapterConfiguration {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(Collections.singletonList(new MappingJackson2HttpMessageConverter()));
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            request.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            return execution.execute(request, body);
+        });
+        return restTemplate;
     }
 }
