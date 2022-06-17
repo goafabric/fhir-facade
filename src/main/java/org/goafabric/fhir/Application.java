be46a8f8c95336968521f476d599fbf9e576db77
Andreas Mautsch <amautsch>
2022-06-17 13:56:50 +0200

    security config updated

--- src/main/java/org/goafabric/fhir/Application.java
+++ src/main/java/org/goafabric/fhir/Application.java
@@ -1,18 +1,15 @@
 package org.goafabric.fhir;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
@@ -22,9 +19,12 @@ public class Application {
     @Configuration @EnableWebSecurity
     @ConditionalOnProperty(value = "security.authentication.enabled", matchIfMissing = true)
     static class SecurityConfiguration {
+
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
+            http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
+                    .and().formLogin().disable()
+                    .csrf().disable();
             return http.build();
         }
     }
