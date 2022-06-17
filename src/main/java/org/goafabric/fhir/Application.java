0e09f140cfd8fde388fcc3fde16610f428b2e71b
Andreas Mautsch <amautsch>
2022-06-17 14:06:42 +0200

    back to the old security fuck u hapi fhir !!

--- src/main/java/org/goafabric/fhir/Application.java
+++ src/main/java/org/goafabric/fhir/Application.java
@@ -1,15 +1,19 @@
 package org.goafabric.fhir;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
@@ -18,14 +22,15 @@ public class Application {
 
     @Configuration @EnableWebSecurity
     @ConditionalOnProperty(value = "security.authentication.enabled", matchIfMissing = true)
-    static class SecurityConfiguration {
+    static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin").password("admin").roles("standard_role");
+        }
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
-                    .and().formLogin().disable()
-                    .csrf().disable();
-            return http.build();
+        @Override
+        protected void configure(final HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
         }
     }
 }
