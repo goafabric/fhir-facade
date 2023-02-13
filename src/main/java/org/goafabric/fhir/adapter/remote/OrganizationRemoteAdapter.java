95eb1a5893a021877b7da98800d1fff12f2d765d
Andreas Mautsch <amautsch>
2023-02-13 08:30:01 +0100

    changes

--- src/main/java/org/goafabric/fhir/adapter/remote/OrganizationRemoteAdapter.java
+++ src/main/java/org/goafabric/fhir/adapter/remote/OrganizationRemoteAdapter.java
@@ -1,16 +1,21 @@
 package org.goafabric.fhir.adapter.remote;
 
 import org.goafabric.fhir.adapter.OrganizationAdapter;
+import org.goafabric.fhir.adapter.mock.OrganizationMockAdapter;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Organization;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Profile("remote")
 @Component
 public class OrganizationRemoteAdapter implements OrganizationAdapter {
+    @Autowired
+    private OrganizationMockAdapter organizationMockAdapter;
+
     @Override
     public Organization getOrganization(IdType idType) {
-        throw new UnsupportedOperationException("NYI");
+        return organizationMockAdapter.getOrganization(idType);
     }
 }
