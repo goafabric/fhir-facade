408de10e68bf04fb0b80fa7bbaf46c90253cc396
Andreas Mautsch <amautsch>
2023-02-14 12:03:07 +0100

    remote adapter added

--- src/main/java/org/goafabric/fhir/adapter/remote/OrganizationRemoteAdapter.java
+++ src/main/java/org/goafabric/fhir/adapter/remote/OrganizationRemoteAdapter.java
@@ -3,15 +3,13 @@ package org.goafabric.fhir.adapter.remote;
 import org.goafabric.fhir.adapter.OrganizationAdapter;
 import org.goafabric.fhir.adapter.mock.OrganizationMockAdapter;
 import org.goafabric.fhir.controller.dto.Organization;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Profile("remote")
 @Component
 public class OrganizationRemoteAdapter implements OrganizationAdapter {
-    @Autowired
-    private OrganizationMockAdapter organizationMockAdapter;
+    private OrganizationMockAdapter organizationMockAdapter = new OrganizationMockAdapter();
 
     @Override
     public Organization getOrganization(String idType) {
