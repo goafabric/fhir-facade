20b2dbf7509d0367934269e946a0b3ac3be44332
Andreas Mautsch <amautsch>
2023-02-15 12:10:58 +0100

    changes

--- src/main/java/org/goafabric/fhir/Application.java
+++ src/main/java/org/goafabric/fhir/Application.java
@@ -1,7 +1,9 @@
 package org.goafabric.fhir;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +11,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    public CommandLineRunner init(ApplicationContext context) {
+        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
+    }
+
 
 }
