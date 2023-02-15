d0de0fc4cd20cccdefd079c4fbf41c370e5f797c
Andreas Mautsch <amautsch>
2023-02-15 12:54:19 +0100

    native image

---
+++ src/main/java/org/goafabric/fhir/controller/dto/Extension.java
@@ -0,0 +1,15 @@
+package org.goafabric.fhir.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Extension {
+    private String url;
+    private String valueString;
+}
