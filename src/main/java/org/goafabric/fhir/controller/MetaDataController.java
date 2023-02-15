fd94049cfded7a89b1ea684a601ad4a5be0f3b60
Andreas Mautsch <amautsch>
2023-02-15 13:49:57 +0100

    extension stuff

--- src/main/java/org/goafabric/fhir/controller/MetaDataController.java
+++ src/main/java/org/goafabric/fhir/controller/MetaDataController.java
@@ -1,17 +1,22 @@
 package org.goafabric.fhir.controller;
 
-import org.goafabric.fhir.controller.dto.Meta;
+import org.goafabric.fhir.controller.dto.metadata.MetaData;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping(value = "fhir/metadata", produces = {MediaType.APPLICATION_JSON_VALUE, "application/fhir+json"})
 public class MetaDataController {
 
     @GetMapping
-    public Meta getMetadata() {
-        return new Meta();
+    public MetaData getMetadata() {
+        return MetaData.builder()
+                .date(new Date().toString())
+                .fhirVersion("CapabilityStatement")
+                .build();
     }
 }
