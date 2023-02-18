92b329adb95a117f39752a6fdab54d8fe80c3284
Andreas Mautsch <amautsch>
2023-02-18 10:37:44 +0100

    changes

--- doc/todo.md
+++ doc/todo.md
@@ -2,6 +2,7 @@ Todos
 - Extension
 => This is currently not working 100% due to Strings beeing Wrappers with value field, see JacksonConfig for details
 - BundleSearch with XML, because this wants geschachtelt XML with inner Type (e.g. "Patient") 
+- BundleSearch with Json return LinkedList instead of Object
 
 Differences to HAPI Fhir
 - Need to generate pojos / dtos on your own with Data from HAPI FHIR Facade, e.g with https://www.jsonschema2pojo.org/
--- src/test/java/org/goafabric/fhir/pojo/r4/XMLTests.java
+++ src/test/java/org/goafabric/fhir/pojo/r4/XMLTests.java
@@ -2,10 +2,16 @@ package org.goafabric.fhir.pojo.r4;
 
 import ca.uhn.fhir.context.FhirContext;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import org.goafabric.fhir.adapter.mock.PatientMockAdapter;
+import org.goafabric.fhir.controller.dto.types.StringType;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.Test;
 
@@ -24,20 +30,42 @@ public class XMLTests {
         assertThat(patient).isNotNull();
 
         //for this to work all Strings need @JsonSerialize(converter = StringConverter.class)
-        //assertThat(patient.getName().get(0).getFamily()).isEqualTo("Simpson");
-        //assertThat(patient.getName().get(0).getGiven().get(0).getValue()).isEqualTo("Homer");
+        assertThat(patient.getName().get(0).getFamily()).isEqualTo("Simpson");
+        assertThat(patient.getName().get(0).getGiven().get(0).getValue()).isEqualTo("Homer");
     }
 
     private static String getXml() throws JsonProcessingException {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(new StringSerializer(null));
         var xmlMapper = XmlMapper.builder()
                 //has to be in line with JacksonConfig Configuration
                 .defaultUseWrapper(false)
                 .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .addModule(module)
                 .build();
 
         xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
         String xml = xmlMapper.writeValueAsString(new PatientMockAdapter().getPatient("1"));
         System.err.println(xml);
         return xml;
+    }
+
+    static class StringSerializer extends StdSerializer<String> {
+
+        protected StringSerializer(Class<String> t) {
+            super(t);
+        }
+
+        @Override
+        public void serialize(String value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            if (gen instanceof ToXmlGenerator) { //only hack if xml and not json
+                gen.writeObject(new StringType(value));
+            } else {
+                gen.writeString(value);
+            }
+        }
+        public Class<String> handledType() {
+            return String.class;
+        }
     }
 }
