c00811f1a6b63fe160a5d71554160dbccb8c5f46
Andreas Mautsch <amautsch>
2023-02-20 09:26:52 +0100

    changes

--- src/test/java/org/goafabric/fhir/pojo/r4/adapter/FhirAdapterPojoIT.java
+++ src/test/java/org/goafabric/fhir/pojo/r4/adapter/FhirAdapterPojoIT.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
