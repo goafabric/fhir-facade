52c9b39f3343ec19b4823ee921187486f10c43e3
Andreas Mautsch <amautsch>
2024-05-25 10:32:07 +0200

    upgrade

--- src/main/java/org/goafabric/fhir/Application.java
+++ src/main/java/org/goafabric/fhir/Application.java
@@ -1,11 +1,22 @@
 package org.goafabric.fhir;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(Application.ApplicationRuntimeHints.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.resources().registerPattern("ca/uhn/fhir/i18n/hapi-messages.properties");
+        }
     }
 }
